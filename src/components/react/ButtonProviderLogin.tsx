8d70e9b23fff82a745d2413d7a1f4556621355c3
Crisops <crialeperez1835@gmail.com>
2024-07-21 21:40:07 -0500

    STYLE group hover elements children

--- src/components/react/ButtonProviderLogin.tsx
+++ src/components/react/ButtonProviderLogin.tsx
@@ -7,7 +7,7 @@ interface ProviderComponentProps {
 }
 function ButtonProviderLogin ({ name, provider, ProviderLogin }: ProviderComponentProps): JSX.Element {
   return (
-    <button className='flex justify-center items-center size-14 bg-zinc-200/50 rounded-full shadow-[0px_1px_10px] shadow-zinc-600/30 transition-shadow duration-300 hover:shadow-md hover:shadow-zinc-300' name={name} value={provider} type='submit'><ProviderLogin /></button>
+    <button className='group flex justify-center items-center size-14 bg-zinc-200/50 rounded-full shadow-[0px_1px_10px] shadow-zinc-600/30 transition-shadow duration-300 hover:shadow-md hover:shadow-zinc-300' name={name} value={provider} type='submit'><ProviderLogin /></button>
   )
 }
 
