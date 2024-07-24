db1cd9f012f04118857c842802e8acfe9172bd8d
Crisops <crialeperez1835@gmail.com>
2024-07-24 10:32:56 -0500

    Add function handles to sign in with provider o account personal. Add property signInProvider to Button provider

--- src/components/react/FormSignIn.tsx
+++ src/components/react/FormSignIn.tsx
@@ -1,7 +1,10 @@
+
+import type { FormEvent } from 'react'
 import { useFormStore } from '../store/useFormStore'
 import ButtonProviderLogin from './ButtonProviderLogin'
 import { Facebook, GitHub, Google } from './Icons'
 import Input from './Input'
+import { handleSubmit, handleSignInProvider } from '../../helpers/fetchSignIn'
 
 function FormSignIn (): JSX.Element {
   const { setWalkForm } = useFormStore(state => state)
@@ -10,15 +13,19 @@ function FormSignIn (): JSX.Element {
     setWalkForm(true)
   }
 
+  const SignIn = (e: FormEvent<HTMLFormElement>): void => {
+    handleSubmit(e).catch(err => console.log(err))
+  }
+
   return (
     <div className='absolute top-0 left-0 w-1/2 h-full flex justify-center'>
-      <form className='w-96 h-full flex flex-col justify-center'>
+      <form onSubmit={SignIn} className='w-96 h-full flex flex-col justify-center'>
         <div className='flex flex-col justify-center items-center gap-y-4'>
           <h1 className='text-4xl font-bold text-center'>Sign in</h1>
           <div className='flex gap-x-6'>
-            <ButtonProviderLogin name='provider' provider='github' ProviderLogin={GitHub} />
-            <ButtonProviderLogin name='provider' provider='google' ProviderLogin={Google} />
-            <ButtonProviderLogin name='provider' provider='facebook' ProviderLogin={Facebook} />
+            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='github' ProviderLogin={GitHub} />
+            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='google' ProviderLogin={Google} />
+            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='facebook' ProviderLogin={Facebook} />
           </div>
           <div>
             <span className='text-zinc-400 text-sm'>or use your account</span>
