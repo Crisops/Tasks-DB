7ee7cf9a0e46bc8c78afd349301cc937b84144fe
Crisops <crialeperez1835@gmail.com>
2024-07-24 10:35:45 -0500

    Delete property name and add property function sign in provider in interface. Add event onClick the button and delete attribute type with value submit

--- src/components/react/ButtonProviderLogin.tsx
+++ src/components/react/ButtonProviderLogin.tsx
@@ -1,13 +1,19 @@
+import type { MouseEvent } from 'react'
 import type React from 'react'
 
 interface ProviderComponentProps {
+  signInProvider: (e: MouseEvent<HTMLButtonElement>) => Promise<void>
   ProviderLogin: React.ComponentType
-  name: string
   provider: string
 }
-function ButtonProviderLogin ({ name, provider, ProviderLogin }: ProviderComponentProps): JSX.Element {
+
+function ButtonProviderLogin ({ signInProvider, provider, ProviderLogin }: ProviderComponentProps): JSX.Element {
+  const handleClick = (e: MouseEvent<HTMLButtonElement>): void => {
+    signInProvider(e).catch(err => console.log(err))
+  }
+
   return (
-    <button className='group flex justify-center items-center size-14 bg-zinc-200/50 rounded-full shadow-[0px_1px_10px] shadow-zinc-600/30 transition-shadow duration-300 hover:shadow-md hover:shadow-zinc-300' name={name} value={provider} type='submit'><ProviderLogin /></button>
+    <button onClick={handleClick} className='group flex justify-center items-center size-14 bg-zinc-200/50 rounded-full shadow-[0px_1px_10px] shadow-zinc-600/30 transition-shadow duration-300 hover:shadow-md hover:shadow-zinc-300' name={provider} value={provider}><ProviderLogin /></button>
   )
 }
 
