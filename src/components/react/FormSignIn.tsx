4847cb3706cf7bdf3025fea5c1c2889d1233e2fd
Crisops <crialeperez1835@gmail.com>
2024-07-24 19:20:04 -0500

    Refactory code where add useState for controllig the value input provider and props Button Provider

--- src/components/react/FormSignIn.tsx
+++ src/components/react/FormSignIn.tsx
@@ -1,16 +1,22 @@
 
-import type { FormEvent } from 'react'
+import { useState, type FormEvent } from 'react'
 import { useFormStore } from '../store/useFormStore'
 import ButtonProviderLogin from './ButtonProviderLogin'
 import { Facebook, GitHub, Google } from './Icons'
 import Input from './Input'
-import { handleSubmit, handleSignInProvider } from '../../helpers/fetchSignIn'
+import { handleSubmit } from '../../helpers/fetchSignIn'
 
 function FormSignIn (): JSX.Element {
   const { setWalkForm } = useFormStore(state => state)
 
+  const [provider, setProvider] = useState('')
+
   const handleSignUp = (): void => {
     setWalkForm(true)
+  }
+
+  const selectProvider = (provider: string): void => {
+    setProvider(provider)
   }
 
   const SignIn = (e: FormEvent<HTMLFormElement>): void => {
@@ -20,12 +26,13 @@ function FormSignIn (): JSX.Element {
   return (
     <div className='absolute top-0 left-0 w-1/2 h-full flex justify-center'>
       <form onSubmit={SignIn} className='w-96 h-full flex flex-col justify-center'>
+        <input onChange={() => selectProvider} hidden name='provider' value={provider} />
         <div className='flex flex-col justify-center items-center gap-y-4'>
           <h1 className='text-4xl font-bold text-center'>Sign in</h1>
           <div className='flex gap-x-6'>
-            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='github' ProviderLogin={GitHub} />
-            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='google' ProviderLogin={Google} />
-            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='facebook' ProviderLogin={Facebook} />
+            <ButtonProviderLogin signInProvider={selectProvider} provider='github' ProviderLogin={GitHub} />
+            <ButtonProviderLogin signInProvider={selectProvider} provider='google' ProviderLogin={Google} />
+            <ButtonProviderLogin signInProvider={selectProvider} provider='facebook' ProviderLogin={Facebook} />
           </div>
           <div>
             <span className='text-zinc-400 text-sm'>or use your account</span>
--- src/components/react/FormSignUp.tsx
+++ src/components/react/FormSignUp.tsx
@@ -1,13 +1,14 @@
+import { useState, type FormEvent } from 'react'
 import { useFormStore } from '../store/useFormStore'
 import ButtonProviderLogin from './ButtonProviderLogin'
 import { Facebook, GitHub, Google } from './Icons'
 import Input from './Input'
-import { handleSignInProvider } from '../../helpers/fetchSignIn'
 import { handleSubmit } from '../../helpers/fetchSignUp'
-import type { FormEvent } from 'react'
 
 function FormSignUp (): JSX.Element {
   const { setWalkForm } = useFormStore(state => state)
+
+  const [provider, setProvider] = useState('')
 
   const handleSignIn = (): void => {
     setWalkForm(false)
@@ -17,15 +18,20 @@ function FormSignUp (): JSX.Element {
     handleSubmit(e).catch(err => console.log(err))
   }
 
+  const selectProvider = (provider: string): void => {
+    setProvider(provider)
+  }
+
   return (
     <div className='absolute top-0 right-0 w-1/2 h-full flex justify-center'>
       <form onSubmit={SignUp} className='w-96 h-full flex flex-col justify-center'>
+        <input onChange={() => selectProvider} hidden name='provider' value={provider} />
         <div className='flex flex-col justify-center items-center gap-y-4'>
           <h1 className='text-4xl font-bold text-center'>Create account</h1>
           <div className='flex gap-x-6'>
-            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='github' ProviderLogin={GitHub} />
-            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='google' ProviderLogin={Google} />
-            <ButtonProviderLogin signInProvider={handleSignInProvider} provider='facebook' ProviderLogin={Facebook} />
+            <ButtonProviderLogin signInProvider={selectProvider} provider='github' ProviderLogin={GitHub} />
+            <ButtonProviderLogin signInProvider={selectProvider} provider='google' ProviderLogin={Google} />
+            <ButtonProviderLogin signInProvider={selectProvider} provider='facebook' ProviderLogin={Facebook} />
           </div>
           <div>
             <span className='text-zinc-400 text-sm'>or use your email and password</span>
