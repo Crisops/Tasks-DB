d0cdabe274cd600ed76f1f30390ba61678d97c96
Crisops <crialeperez1835@gmail.com>
2024-07-24 19:21:39 -0500

    Refactory code where delete function Mouse Event and change for function void

--- src/components/react/ButtonProviderLogin.tsx
+++ src/components/react/ButtonProviderLogin.tsx
@@ -1,19 +1,14 @@
-import type { MouseEvent } from 'react'
 import type React from 'react'
 
 interface ProviderComponentProps {
-  signInProvider: (e: MouseEvent<HTMLButtonElement>) => Promise<void>
+  signInProvider: (provider: string) => void
   ProviderLogin: React.ComponentType
   provider: string
 }
 
 function ButtonProviderLogin ({ signInProvider, provider, ProviderLogin }: ProviderComponentProps): JSX.Element {
-  const handleClick = (e: MouseEvent<HTMLButtonElement>): void => {
-    signInProvider(e).catch(err => console.log(err))
-  }
-
   return (
-    <button onClick={handleClick} className='group flex justify-center items-center size-14 bg-zinc-200/50 rounded-full shadow-[0px_1px_10px] shadow-zinc-600/30 transition-shadow duration-300 hover:shadow-md hover:shadow-zinc-300' name={provider} value={provider}><ProviderLogin /></button>
+    <button onClick={() => signInProvider(provider)} className='group flex justify-center items-center size-14 bg-zinc-200/50 rounded-full shadow-[0px_1px_10px] shadow-zinc-600/30 transition-shadow duration-300 hover:shadow-md hover:shadow-zinc-300'><ProviderLogin /></button>
   )
 }
 
